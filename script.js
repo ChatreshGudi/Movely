33f61a73bf6ccf0a3bbd882ec235d036ba23674f
Chatresh <chatreshrsg0202@gmail.com>
2022-04-02 21:53:44 +0530

    blockly editor initialized

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+let toolbox = {
+    "kind": "categoryToolbox",
+    "contents": [
+      {
+        "kind": "category",
+        "name": "Control",
+        "contents": [
+          {
+            "kind": "block",
+            "type": "controls_if"
+          },
+        ]
+      },
+      {
+        "kind": "category",
+        "name": "Logic",
+        "contents": [
+          {
+            "kind": "block",
+            "type": "logic_compare"
+          },
+          {
+            "kind": "block",
+            "type": "logic_operation"
+          },
+          {
+            "kind": "block",
+            "type": "logic_boolean"
+          }
+        ]
+      }
+    ]
+  };
+Blockly.inject('blockEdit', {toolbox: toolbox});
+
 function resetCanvas(){
     canvas = document.getElementById("canvas");
     canvas.innerHTML = "";
